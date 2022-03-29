ff9ec89870ae99518e13d8ab0764af5001730e34
orsica30 <orlandojsierrac@gmail.com>
2022-03-29 08:23:41 -0500

    Feat: Interfaz de create-profile

--- src/app/views/CreateProfile/index.js
+++ src/app/views/CreateProfile/index.js
@@ -1,4 +1,22 @@
-import React, { useState } from "react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import history from '../../../components/History';
+
+
+
+// import React, { useState } from "react";
 // import { Form, InputGroup, Button, Alert } from "react-bootstrap";
 // import { Formik } from "formik";
 // import * as Yup from "yup";
@@ -40,7 +58,7 @@ import React, { useState } from "react";
 //     }),
 // });
 
-export const CreateProfile = () => {
+// export const CreateProfile = () => {
 //   const [disabledButton, setDisabledButton] = useState(false);
 
 //   const onSubmit = (event) => {
@@ -98,8 +116,8 @@ export const CreateProfile = () => {
 //       });
 //   };
 
-  return (
-    <>
+  // return (
+  //   <>
       {/* <div className="container-login">
         <div className="d-flex justify-content-center">
           <div className="card-sign-up">
@@ -319,7 +337,143 @@ export const CreateProfile = () => {
           </div>
         </div>
       </div> */}
-      <div>Estamos en Create Profile</div>
+      {/* <div>Estamos en Create Profile</div>
     </>
   );
-};
+}; */}
+
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}></Typography>
+  );
+}
+
+const theme = createTheme();
+
+export const CreateProfile = ()=>{
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  autoComplete="off"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Full Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="profile.stdicompany.com/username"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Type the serial number"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Confirm Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link onClick={()=>{ history.push('/login') }} variant="body2" sx={{ cursor: 'pointer' }}>
+                  Already have an account? Sign in
+                </Link>                
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+
+
