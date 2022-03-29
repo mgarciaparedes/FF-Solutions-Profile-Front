a0a85ad455686dbd6167f02499f9db92720bf57e
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2022-03-28 23:42:52 -0300

    feat: sass

--- src/app/views/Login/index.js
+++ src/app/views/Login/index.js
@@ -1,320 +1,140 @@
-import React, { useState, useContext } from "react";
-import { Container, Grid } from "@mui/material";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import TextField from '@mui/material/TextField';
-import img from '../../../assets/images/Logo-Apple.jpg';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import history from "../../../components/History";
 
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-// import { Form, InputGroup, Button } from "react-bootstrap";
-// import { Formik } from "formik";
-// import * as Yup from "yup";
-// import Swal from "sweetalert2";
-// import history from "../../../components/History";
-// import LogoWhite from "../../../assets/images/logo-white.png";
-// import * as Icon from "react-bootstrap-icons";
-// import axios from "axios";
-// import helpers from "../../../components/Helpers";
-// import { AppContext } from "../../../components/AppContext";
-
-// const { swalOffBackend } = helpers;
-
-// const schema = Yup.object({
-//   email: Yup.string()
-//     .required("Email is required")
-//     .email("Incorrect email format"),
-//   password: Yup.string()
-//     .required("Password is required")
-//     .matches(/^\S*$/, "Password can't have spaces."),
-// });
-
-
+const theme = createTheme();
 
 export const Login = () => {
-
-
-  //   const { loginContext } = useContext(AppContext);
-
-  //   const [disabledButton, setDisabledButton] = useState(false);
-
-  //   const onSubmit = (event) => {
-  //     const { email, password } = event;
-
-  //     setDisabledButton(true);
-
-  //     const payload = {
-  //       email: email,
-  //       password: password,
-  //     };
-
-  //     axios
-  //       .post(`/auth/login`, payload)
-  //       .then((res) => {
-  //         const { ok, msg, token, name, userid } = res.data;
-  //         //Capturamos el token y lo dejamos en la cabecera
-  //         axios.defaults.headers.common["x-token"] = res.data.token;
-
-  //         /*Sí el login es ok, loguea*/
-  //         if (ok && msg === "login") {
-  //           axios.get(`/users/getProfileUserData`).then((res2) => {
-  //             const {
-  //               ok,
-  //               msg,
-  //               serialNumber,
-  //               username,
-  //               email,
-  //               data,
-  //               gallery,
-  //               customImage,
-  //             } = res2.data;
-  //             /*Sí el login es ok, loguea*/
-  //             if (ok) {
-  //               setDisabledButton(false);
-
-  //               const json = {
-  //                 authenticated: true,
-  //                 user: name,
-  //                 token: token,
-  //                 email: email,
-  //                 serialNumber: serialNumber,
-  //                 username: username,
-  //                 profileData: data,
-  //                 galleryImages:
-  //                   gallery && gallery.galleryImages
-  //                     ? gallery.galleryImages
-  //                     : null,
-  //                 galleryActive:
-  //                   gallery && gallery.galleryActive
-  //                     ? gallery.galleryActive
-  //                     : null,
-  //                 customImage: customImage ? customImage : null,
-  //                 sendNotifications: data ? data.sendNotifications : false,
-  //                 isLinked: data ? data.isLinked : false,
-  //                 usernameLinked: data ? data.usernameLinked : "",
-  //               };
-  //               loginContext(json);
-
-  //               history.push("/dashboard");
-  //             }
-  //           });
-  //         }
-  //       })
-  //       .catch((e) => {
-  //         /*Sí los servicios están OFF, retornamos este swal*/
-  //         if (e.response === undefined) {
-  //           swalOffBackend();
-  //           setDisabledButton(false);
-  //           return 1;
-  //         }
-
-  //         /*Si ocurre algo en el request, retoramos esto*/
-  //         const { msg, ok } = e.response.data;
-  //         if (!ok) {
-  //           Swal.fire({
-  //             title: "Error",
-  //             text: msg,
-  //             icon: "error",
-  //             confirmButtonText: "Try again",
-  //           });
-  //         }
-
-  //         setDisabledButton(false);
-  //       });
-  //   };
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
   return (
-    <>
-      <img src={img}/>      
-
-
-
-      <Grid justifyContent='center' container sx={ {
-        marginTop: 20        
-        } }>
-        <Grid item xs={4} sx={ { paddingBottom: 5, paddingTop: 2, paddingX: 6 } }>
-          <Grid
-            component="form"        
-            autoComplete="off"   
-            justifySelf='center'
-            textAlign='center'                 
-          >      
-          <TextField id="standard-basic" label="Email@gmail.com" variant="standard" xs={12}/>                   
-          </Grid>
-
-          <Grid
-            component="form"        
-            autoComplete="off"
-            textAlign='center'
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-          <TextField id="standard-basic" label="Password" variant="standard" type='password'/>          
-          </Grid>
-
-          <Grid marginTop={2}>
-            <Button variant="text">Submit</Button>
-            <Button variant="text" color="secondary">Remove</Button>
-          </Grid>
-          <Grid sx={ { paddingTop: 3 } }>
-            <Link
-              component="button"
-              variant="body3"            
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
             >
-              Sign up
-            </Link>
-          </Grid>
-          <Grid>
-            <Link
-              component="button"
-              variant="body3"            
-            >
-              Forgot Password
-            </Link>
-          </Grid>
-          <Grid>
-            <Link
-              component="button"
-              variant="body3"              
-              onClick={ ()=> history.push('/create-profile') }            
-            >
-              Create your profile
-            </Link>
-          </Grid>     
-
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link onClick={() => history.push('/create-profile')} variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
         </Grid>
       </Grid>
-      
-
-
-      
-      
-      {/* <div className="container-login">
-        <div className="d-flex justify-content-center">
-          <div className="card-login">
-            <div className="card-header">
-              <h3>Sign in</h3>
-              <div className="d-flex justify-content-end social_icon">
-                <img className="logo-login" src={LogoWhite} alt="Logo" />
-              </div>
-            </div>
-            <div className="card-body">
-              <Formik
-                validationSchema={schema}
-                // onSubmit={(values, { resetForm }) => {
-                //   onSubmit(values);
-                //   resetForm({ values: null });
-                // }}
-                onSubmit={onSubmit}
-                initialValues={{
-                  //email: "miguelgarciaparedes22@gmail.com",
-                  //password: "12345678",
-                  email: "",
-                  password: "",
-                }}
-              >
-                {({
-                  handleSubmit,
-                  handleChange,
-                  handleBlur,
-                  setFieldValue,
-                  values,
-                  touched,
-                  isValid,
-                  errors,
-                }) => (
-                  <Form
-                    onSubmit={handleSubmit}
-                    noValidate
-                    autoComplete="off"
-                    name="addServiceData"
-                    id="addServiceData"
-                  >
-                    <InputGroup className="mb-2">
-                      <InputGroup.Prepend>
-                        <InputGroup.Text>
-                          <Icon.PersonBoundingBox />
-                        </InputGroup.Text>
-                      </InputGroup.Prepend>
-                      <Form.Control
-                        type="text"
-                        placeholder="Example: johndoe@example.com"
-                        name="email"
-                        value={values.email}
-                        onChange={handleChange}
-                        isValid={!!touched.email && !errors.email}
-                        isInvalid={!!errors.email && !!touched.email}
-                        className="lowercase"
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.email}
-                      </Form.Control.Feedback>
-                    </InputGroup>
-                    <InputGroup className="mb-2">
-                      <InputGroup.Prepend>
-                        <InputGroup.Text>
-                          <Icon.KeyFill />
-                        </InputGroup.Text>
-                      </InputGroup.Prepend>
-                      <Form.Control
-                        type="password"
-                        placeholder="Enter your password"
-                        name="password"
-                        value={values.password}
-                        onChange={handleChange}
-                        isValid={!!touched.password && !errors.password}
-                        isInvalid={!!errors.password && !!touched.password}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.password}
-                      </Form.Control.Feedback>
-                    </InputGroup>
-                    <InputGroup>
-                      <Button
-                        type="submit"
-                        variant="primary"
-                        disabled={disabledButton === true}
-                        block
-                      >
-                        <div className="d-flex d-inline-block justify-content-center">
-                          <span
-                            className="spinner-border spinner-border-sm mt-1 mr-2"
-                            role="status"
-                            style={{
-                              display:
-                                disabledButton === true
-                                  ? "inline-block"
-                                  : "none",
-                            }}
-                            aria-hidden="true"
-                          ></span>
-                          {disabledButton === true
-                            ? " Loading, please wait..."
-                            : "Login"}
-                        </div>
-                      </Button>
-                    </InputGroup>
-                  </Form>
-                )}
-              </Formik>
-            </div>
-            <div className="card-footer">
-              <div className="d-flex justify-content-center text-white">
-                Don't have an account? &nbsp;
-                <a href="/create-profile"> Sign Up</a>
-              </div>
-              <div className="d-flex justify-content-center">
-                <a href="/forgot-password">Forgot your password?</a>
-              </div>
-              {/* <div className="d-flex justify-content-center">
-                <a href="/">Privacy Policy and Terms of Service</a>
-              </div> */}
-      {/*</div>
-          </div>
-        </div>
-      </div> */}
-      Estamos en Login
-    </>
+    </ThemeProvider>
   );
 };
