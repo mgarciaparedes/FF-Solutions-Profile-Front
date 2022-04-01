ee6051d3ac30d7ba2bdd133466e1fff147dc8f8b
orsica30 <orlandojsierrac@gmail.com>
2022-04-01 13:26:30 -0500

    fixed: Carousel en dashboard

--- src/app/views/Dashboard/index.js
+++ src/app/views/Dashboard/index.js
@@ -41,214 +41,37 @@ export const Dashboard = (props) => {
 //     setIndex(selectedIndex);
 //   };
 
-if (props.newProp) console.log(props.newProp);
-  const contentPosition = props.contentPosition
-    ? props.contentPosition
-    : "left";
-  const totalItems = props.length ? props.length : 3;
-  const mediaLength = totalItems - 1;
-
-  let items = [];
-  const content = (
-    <Grid item xs={12 / totalItems} key="content">
-      <CardContent className="Content">
-        <Typography className="Title">{props.item.Name}</Typography>
-
-        <Typography className="Caption">{props.item.Caption}</Typography>
-
-        <Button variant="outlined" className="ViewButton">
-          View Now
-        </Button>
-      </CardContent>
-    </Grid>
-  );
-
-  for (let i = 0; i < mediaLength; i++) {
-    const item = props.item.Items[i];
-
-    const media = (
-      <Grid item xs={12 / totalItems} key={item.Name}>
-        <CardMedia className="Media" image={item.Image} title={item.Name}>
-          <Typography className="MediaCaption">{item.Name}</Typography>
-        </CardMedia>
-      </Grid>
-    );
-
-    items.push(media);
-  }
-
-  if (contentPosition === "left") {
-    items.unshift(content);
-  } else if (contentPosition === "right") {
-    items.push(content);
-  } else if (contentPosition === "middle") {
-    items.splice(items.length / 2, 0, content);
-  }
-
-  return (
-    <Card raised className="Banner">
-      <Grid container spacing={0} className="BannerGrid">
-        {items}
-      </Grid>
-    </Card>
-  );
-}
-
-const items = [
-  {
-    Name: "Pizza begin",
-    Image: "https://source.unsplash.com/featured/?macbook",
-    contentPosition: "left",
-    Items: [
-      {
-        Name: "Macbook Pro",
-        Image: "https://source.unsplash.com/featured/?macbook"
-      },
-      {
-        Name: "iPhone",
-        Image: "https://source.unsplash.com/featured/?iphone"
-      }
-    ]
-  },
-  {
-    Name: "Home Appliances",
-    Caption: "Say no to manual home labour!",
-    contentPosition: "middle",
-    Items: [
-      {
-        Name: "Washing Machine WX9102",
-        Image: "https://source.unsplash.com/featured/?washingmachine"
-      },
-      {
-        Name: "Learus Vacuum Cleaner",
-        Image: "https://source.unsplash.com/featured/?vacuum,cleaner"
-      }
-    ]
-  },
-  {
-    Name: "Decoratives",
-    Caption: "Give style and color to your living room!",
-    contentPosition: "right",
-    Items: [
-      {
-        Name: "Living Room Lamp",
-        Image: "https://source.unsplash.com/featured/?lamp"
-      },
-      {
-        Name: "Floral Vase",
-        Image: "https://source.unsplash.com/featured/?vase"
-      }
-    ]
-  }
-];
-
-class BannerExample extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      autoPlay: true,
-      animation: "fade",
-      indicators: true,
-      timeout: 500,
-      navButtonsAlwaysVisible: false,
-      navButtonsAlwaysInvisible: false,
-      cycleNavigation: true
-    };
-
-    autoBind(this);
-  }
-
-  toggleAutoPlay() {
-    this.setState({
-      autoPlay: !this.state.autoPlay
-    });
-  }
-
-  toggleIndicators() {
-    this.setState({
-      indicators: !this.state.indicators
-    });
-  }
-
-  toggleNavButtonsAlwaysVisible() {
-    this.setState({
-      navButtonsAlwaysVisible: !this.state.navButtonsAlwaysVisible
-    });
-  }
-
-  toggleNavButtonsAlwaysInvisible() {
-    this.setState({
-      navButtonsAlwaysInvisible: !this.state.navButtonsAlwaysInvisible
-    });
-  }
-
-  toggleCycleNavigation() {
-    this.setState({
-      cycleNavigation: !this.state.cycleNavigation
-    });
-  }
-
-  changeAnimation(event) {
-    this.setState({
-      animation: event.target.value
-    });
-  }
-
-  changeTimeout(event, value) {
-    this.setState({
-      timeout: value
-    });
-  }
-
-
-  render() {
+const contentStyle = {    
+  height: '400px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+}; 
   return (
     <>
-      <div style={{ marginTop: "50px", color: "#494949" }}>
-        <h2>3 Items layout - StackOverflow - Yotam</h2>
+      <Grid container justifyContent='center'>
+        <Grid item xs={8} >
+          <Carousel autoplay>
+            <Grid justifyItems='center'>
+              <Grid style={contentStyle}>
+                1                
+              </Grid>
+            </Grid>        
+            <Grid>
+              <Grid style={contentStyle}>
+                2                
+              </Grid>
+            </Grid>
+            <Grid>
+              <Grid style={contentStyle}>
+                3                
+              </Grid>
+            </Grid>  
+          </Carousel>
 
-        <Carousel
-          className="Example"
-          autoPlay={this.state.autoPlay}
-          animation={this.state.animation}
-          indicators={this.state.indicators}
-          timeout={this.state.timeout}
-          cycleNavigation={this.state.cycleNavigation}
-          navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
-          navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
-          next={(now, previous) =>
-            console.log(
-              `Next User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          prev={(now, previous) =>
-            console.log(
-              `Prev User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          onChange={(now, previous) =>
-            console.log(
-              `OnChange User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          // fullHeightHover={false}
-          // navButtonsProps={{style: {backgroundColor: 'cornflowerblue', borderRadius: 0}}}
-          // navButtonsWrapperProps={{style: {bottom: '0', top: 'unset', }}}
-          // indicatorContainerProps={{style: {margin: "20px"}}}
-          // NextIcon='next'
-        >
-          {items.map((item, index) => {
-            return (
-              <Dashboard
-                item={item}
-                key={index}
-                contentPosition={item.contentPosition}
-              />
-            );
-          })}
-        </Carousel>
-        </div>           
+        </Grid>
+      </Grid>           
         
       {/* <div className="container mt-3 pr-4">
         <div className="row">
@@ -357,5 +180,5 @@ class BannerExample extends React.Component {
             
     </>
   );
-}
+
 }
