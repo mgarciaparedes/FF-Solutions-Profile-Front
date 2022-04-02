7d2a5c2045cba2e61d9207f867f0546054249d2e
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2022-04-01 23:05:43 -0300

    feat: implementación de yup y formik para validar asincrónicamente el formulario

--- src/app/views/Login/index.js
+++ src/app/views/Login/index.js
@@ -13,7 +13,22 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import history from "../../../components/History";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
+//Constante con el formato de validación para cada campo-----------------------------------------------------
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(8, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
+});
+
+//Footer de Formulario Login----------------------------------------------------------------------------------
 function Copyright(props) {
   return (
     <Typography
@@ -32,28 +47,42 @@ function Copyright(props) {
   );
 }
 
+//Se crea el tema de plantilla preconfigurado por Material UI para obtener el diseño del Login-----------------
 const theme = createTheme();
 
+//Inicio de componente-----------------------------------------------------------------------------------------
 export const Login = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+  //Setear los valores a evaluar por el formulario-------------------------------------------------------------
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema, //Se le pasa la costante de formato de cada campo
+    onSubmit: (values) => {
+      //Si llega acá es porque pasó todas las validaciones, le enviamos los values
+      //de cada campo en el formulario, se obtienen con el nombre de cada campo
+      //para este caso particular: values.email y values.password
+      signIn(values); 
+    },
+  });
+
+  //Función de reconocimiento de combinación correcta email - password
+  const signIn = (values) => {
+    // alert(JSON.stringify(values, null, 2));
+    alert("Login correcto, inicio de sesión");
+    history.push('/dashboard');
   };
 
-  const signIn = ()=>{
-    
-    history.push('/dashboard')
-
-  }
-
   return (
+    <>
+    {/*Inicio de Tema proporcionado por template de Material UI */}
     <ThemeProvider theme={theme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
+
+        {/* Grilla en donde se muestran las imágenes random */}
         <Grid
           item
           xs={false}
@@ -70,6 +99,8 @@ export const Login = () => {
             backgroundPosition: "center",
           }}
         />
+
+        {/* Grilla en donde se muestra el formulario */}
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
@@ -86,45 +117,64 @@ export const Login = () => {
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box
+            {/* <Box
               component="form"
               noValidate
               onSubmit={handleSubmit}
               sx={{ mt: 1 }}
-            >
+            > */}
+
+            {/* Inicio de formulario */}
+            <form onSubmit={formik.handleSubmit}>
+
+              {/* Campo Email */}
               <TextField
                 margin="normal"
-                required
                 fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
                 autoComplete="email"
                 autoFocus
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
               />
+
+              {/* Campo Password */}
               <TextField
                 margin="normal"
-                required
                 fullWidth
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
               />
-              <FormControlLabel
+
+              {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
-              />
+              /> */}
+
+              {/* Botón submit */}
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
-                onClick={ signIn }
               >
                 Sign In
               </Button>
+
+              {/* Grilla nueva en donde se muestran opciones de crear profile y cambio de contraseña */}
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">
@@ -132,16 +182,25 @@ export const Login = () => {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link onClick={() => history.push('/create-profile')} variant="body2" sx={ { cursor: "pointer" } }>
+                  <Link
+                    onClick={() => history.push("/create-profile")}
+                    variant="body2"
+                    sx={{ cursor: "pointer" }}
+                  >
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
               </Grid>
               <Copyright sx={{ mt: 5 }} />
-            </Box>
+            </form>
+            {/* Final del Formulario */}
+            {/* </Box> */}
           </Box>
         </Grid>
+        {/* Final de Grilla de opciones de crear profile y cambio de contraseña */}
       </Grid>
+      {/* Final de Grilla principal del formulario */}
     </ThemeProvider>
+    </>
   );
 };
